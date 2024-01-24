22c5ca02b22ffd4ed73eec2c8c8190ebf65f45a3
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-24 22:26:16 +0000

    Place the image

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -10,6 +10,31 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      color: const Color.fromARGB(255, 162, 188, 210),
+      child: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Container(
+                width: double.infinity,
+                padding: const EdgeInsets.all(42),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    bottomLeft:
+                        Radius.circular(MediaQuery.of(context).size.width / 2),
+                    bottomRight:
+                        Radius.circular(MediaQuery.of(context).size.width / 2),
+                  ),
+                ),
+                child: Image.asset("images/task_man.png"),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
