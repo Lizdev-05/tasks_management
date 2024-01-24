91599e339765acb13f6e9898c9cbb49c134d0af9
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2024-01-24 21:50:24 +0000

    Implement routing

--- lib/tasks_app.dart
+++ lib/tasks_app.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:tasks_management/dashboard.dart';
+import 'package:tasks_management/splash_screen.dart';
+import 'package:tasks_management/tasks_page.dart';
 
 class TasksApp extends StatelessWidget {
   const TasksApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: "/",
+        routes: <String, WidgetBuilder>{
+          "/": (context) => const SplashScreen(),
+          "/dashboard": (context) => const Dashboard(),
+          "/taskPage": (context) => const TasksPage(),
+        });
   }
 }
